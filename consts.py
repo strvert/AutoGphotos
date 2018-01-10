3774da919179154693aa62fdb35c131e86272ec9
Ir13570 <rikuisikawa11924@gmail.com>
2018-01-11 01:14:05 +0900

    アップロード周り整備

--- consts.py
+++ consts.py
@@ -1,4 +1,5 @@
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 PICASA_URL = "https://picasaweb.google.com/data/"
 TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token"
+ENDPOINT_URL = "https://picasaweb.google.com/data/feed/api/user/"
 TOKEN_PATH = ".save.json"
--- upload.py
+++ upload.py
@@ -1,9 +1,37 @@
+import json
+import requests
+
 from token_utils import Token_utils
+import consts
 
 
 class Upload:
 
     def upload(self, path):
+
+        with open(consts.TOKEN_PATH, 'r') as f:
+            data_dict = json.load(f)
+        user_id = data_dict['user_id']
+
         tu = Token_utils
         access_token = tu.reget_token()
+
+        headers = {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': '${LENGTH',
+            'Slug': '${FILE}',
+        }
+
+        params = (
+            ('access_token', '${ACCESS_TOKEN}'),
+        )
+
+        with open('path', 'rb') as f:
+            data = f
+
+        response = requests.post(consts.ENDPOINT_URL + user_id,
+                headers=headers, params=params, data=data)
+
+        response = requests.post('${ENDPOINT}?'
+                + 'access_token=${ACCESS_TOKEN}', headers=headers, data=data)
 
